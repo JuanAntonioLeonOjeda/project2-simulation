5d5628cdc5832619556ea893135f811e1b0da5f7
root <root@LAPTOP-LMJQULD0.localdomain>
2022-10-13 20:48:13 +0100

    Server working

--- index.js
+++ index.js
@@ -8,12 +8,29 @@ const {
 
 const addRelations = require('./database/relations')
 
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 async function connectToDB () {
   await checkConnection()
   addRelations()
   await syncModels()
 }
 
+function startExpress() {
+  const app = express()
+    .use(cors())
+    .use(morgan('dev'))
+    .use(express.json())
+    .use('/api', require('./api/routes'))
+
+    .listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`)
+    })
+}
+
 (async function startAPI() {
   await connectToDB()
+  startExpress()
 })()
