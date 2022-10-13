f6f7dd9faa2a0762a710aca1a8993d0796a0a3ad
root <root@LAPTOP-LMJQULD0.localdomain>
2022-10-13 20:28:25 +0100

    login function created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,3 +21,32 @@ async function signup (req, res) {
     return res.status(500).send(error.message)
   }
 }
+
+async function login (req, res) {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (!user) return res.status(404).send('Email or password incorrect')
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) return res.status(500).send(err)
+      if (!result) return res.status(404).send('Email or password incorrect')
+
+      const payload = { email: user.email, userName: user.userName }
+      const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' })
+      return res.status(200).json({ token: token })
+    })
+
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  signup,
+  login
+}
