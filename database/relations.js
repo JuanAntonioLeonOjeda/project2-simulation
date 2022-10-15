75bfdb3c67a86ac95a83e4b6af41d7a1e6079944
root <root@LAPTOP-LMJQULD0.localdomain>
2022-10-15 09:26:16 +0100

    Relations completed

--- database/relations.js
+++ database/relations.js
@@ -6,7 +6,15 @@ function addRelations() {
     
     User.hasMany(Tweet)
     Tweet.belongsTo(User)
-    
+
+    User.belongsToMany(User, { as: 'Friend', through: 'Friends' })
+
+    User.belongsToMany(Tweet, { through: 'Favourites' })
+    Tweet.belongsToMany(User, { through: 'Favourites' })
+
+    User.belongsToMany(Tweet, { through: 'Retweets' })
+    Tweet.belongsToMany(User, { through: 'Retweets' })
+
     console.log('Relations added')
   } catch (error) {
     throw error
